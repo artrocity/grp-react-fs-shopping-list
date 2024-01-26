1b7825cce8b7772d597ca516a1a942ae88baf596
Richard Barnitz <richardbarnitz@gmail.com>
2024-01-25 16:19:45 -0800

    added cart icon

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './Header.css';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 
 function Header() {
-    return (
-        <header className="banner-header">
-            <h1>My Shopping List</h1>
-        </header>
-    );
+  return (
+    <header className="banner-header">
+      <h1>My Shopping List</h1>
+      <ShoppingCartIcon />
+    </header>
+  );
 }
 
 export default Header;
